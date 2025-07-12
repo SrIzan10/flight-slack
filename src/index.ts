9644ba8ab895b9e1c44961e3f9cc499420ab284e
SrIzan10 <66965250+SrIzan10@users.noreply.github.com>
2025-07-12 17:38:14 +0000

    feat: get opensky token and cache

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,8 @@
 import { PrismaClient } from "@prisma/client";
+import BunCache from "@samocodes/bun-cache";
 import { App } from "@slack/bolt";
+import ky from "ky";
+import { getOpenskyToken } from "./util/openskyToken";
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
@@ -7,7 +10,16 @@ const app = new App({
   appToken: process.env.SLACK_APP_TOKEN,
 });
 
-const db = new PrismaClient();
+export const db = new PrismaClient();
+export const cache = new BunCache();
+
+const now = Math.floor(Date.now() / 1000);
+const flights = await ky.get(`https://opensky-network.org/api/flights/departure?airport=LEMG&begin=${now - 3600}&end=${now}`, {
+  headers: {
+    'Authorization': `Bearer ${await getOpenskyToken()}`,
+  }
+}).json();
+console.log("Fetched flights:", flights);
 
 await app.start();
 console.log("We're up and running :)");
